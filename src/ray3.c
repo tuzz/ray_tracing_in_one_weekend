a3aae9723dfd5a75dba5bd9a9b2dbbd6393f5192
Chris Patuzzo <chris@patuzzo.co.uk>
2026-02-13 18:26:11 +0000

    Simplify ray_hit_sphere by substituting b = -2h

--- src/ray3.c
+++ src/ray3.c
@@ -10,15 +10,15 @@ static Point3 ray3_at(Ray3 self, float t) {
 static float ray3_hit_sphere(Ray3 self, Point3 center, float radius) {
   Vec3 oc = vec3_subtract(center, self.origin);
 
-  float a = vec3_dot(self.direction, self.direction);
-  float b = -2.0f * vec3_dot(self.direction, oc);
-  float c = vec3_dot(oc, oc) - radius * radius;
-  float discriminant = b * b - 4.0f * a * c;
+  float a = vec3_length_squared(self.direction);
+  float h = vec3_dot(self.direction, oc);
+  float c = vec3_length_squared(oc) - radius * radius;
+  float discriminant = h * h - a * c;
 
   if (discriminant < 0) {
     return -1.0f;
   } else {
-    return (-b - sqrtf(discriminant)) / (2.0f * a);
+    return (h - sqrtf(discriminant)) / a;
   }
 }
 
