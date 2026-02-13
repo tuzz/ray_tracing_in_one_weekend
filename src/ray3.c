baf433f0308d41518a58cfe40bfa0757ebc97342
Chris Patuzzo <chris@patuzzo.co.uk>
2026-02-13 18:21:32 +0000

    Color the sphere based on the normal of the ray intersection point

--- src/ray3.c
+++ src/ray3.c
@@ -7,20 +7,29 @@ static Point3 ray3_at(Ray3 self, float t) {
   return vec3_add(self.origin, vec3_scale(self.direction, t));
 }
 
-static bool ray3_hit_sphere(Ray3 self, Point3 center, float radius) {
+static float ray3_hit_sphere(Ray3 self, Point3 center, float radius) {
   Vec3 oc = vec3_subtract(center, self.origin);
 
   float a = vec3_dot(self.direction, self.direction);
   float b = -2.0f * vec3_dot(self.direction, oc);
   float c = vec3_dot(oc, oc) - radius * radius;
-
   float discriminant = b * b - 4.0f * a * c;
-  return discriminant >= 0.0f;
+
+  if (discriminant < 0) {
+    return -1.0f;
+  } else {
+    return (-b - sqrtf(discriminant)) / (2.0f * a);
+  }
 }
 
 static Color3 ray3_color(Ray3 self) {
-  if (ray3_hit_sphere(self, (Point3){ 0.0f, 0.0f, -1.0f }, 0.5)) {
-    return (Color3){ 1.0f, 0.0f, 0.0f };
+  Point3 sphere_center = { 0.0f, 0.0f, -1.0f };
+  float t = ray3_hit_sphere(self, sphere_center, 0.5);
+
+  if (t > 0.0f) {
+    Point3 sphere_point = ray3_at(self, t);
+    Vec3 normal = vec3_unit(vec3_subtract(sphere_point, sphere_center));
+    return vec3_scale((Vec3){ normal.x + 1.0f, normal.y + 1.0f, normal.z + 1.0f }, 0.5f);
   }
 
   Vec3 unit_direction = vec3_unit(self.direction);
