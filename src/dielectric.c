bd3b31720f120654e5fa3ff16a1b993de4044062
Chris Patuzzo <chris@patuzzo.co.uk>
2026-02-15 13:29:13 +0000

    Approximate reflectivity that varies by angle of incidence

--- src/dielectric.c
+++ src/dielectric.c
@@ -2,6 +2,12 @@ typedef struct {
   float refaction_index;
 } Dielectric;
 
+static float dielectric_reflectance(float cosine, float refraction_index) {
+  float r0 = (1.0f - refraction_index) / (1.0f + refraction_index);
+  r0 = r0 * r0;
+  return r0 + (1.0f - r0) * powf((1.0f - cosine), 5.0f);
+}
+
 static bool dielectric_scatter(const Dielectric *d, const Ray3 *ray, const Hit *hit, Color3 *attentuation, Ray3 *scattered) {
   *attentuation = WHITE;
   float ri = hit->front_face ? (1.0f / d->refaction_index) : d->refaction_index;
@@ -13,7 +19,7 @@ static bool dielectric_scatter(const Dielectric *d, const Ray3 *ray, const Hit *
   bool cannot_refract = ri * sin_theta > 1.0f;
   Vec3 direction;
 
-  if (cannot_refract) {
+  if (cannot_refract || dielectric_reflectance(cos_theta, ri) > random_float()) {
     direction = vec3_reflect(unit_direction, hit->normal);
   } else {
     direction = vec3_refract(unit_direction, hit->normal, ri);
